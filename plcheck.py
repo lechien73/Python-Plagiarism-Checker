be2cf267d738c655a6df9e452f7df7942c9b9942
Matt Rudge <matt.rudge@codeinstitute.net>
2021-11-03 10:32:30 +0000

    Debug to False

--- plcheck.py
+++ plcheck.py
@@ -130,4 +130,4 @@ async def report(uuid):
 
 if __name__ == "__main__":
     app.run(host=os.getenv("IP", "0.0.0.0"), port=int(
-        os.getenv("PORT", "5000")), debug=True)
+        os.getenv("PORT", "5000")), debug=False)
